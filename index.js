6423fa8e3976b9f1d69389ec5b0fcdf970a0e765
Deric Walintukan <deric@loopapps.com>
2022-03-14 11:58:19 -0700

    change regex for release tag and branch

--- index.js
+++ index.js
@@ -62,13 +62,13 @@ const validateReleaseCandidateIssue = (labels) => {
  */
 const parseIssueBody = (body) => {
   // Extract release tag with regex
-  const tagRegex = new RegExp(/-\sRelease\stag:\s(.+)\n/m)
+  const tagRegex = new RegExp(/Release\stag:\s(.+)\n/m)
   const tagMatch = body.match(tagRegex)
-  if (!tagMatch) throw Error('No "Release Tag" found in issue body')
+  if (!tagMatch) throw Error('No "Release tag" found in issue body')
   const tag = tagMatch[1]
 
   // Extract branch with regex
-  const branchRegex = new RegExp(/-\sBranch:\s(.+)\n/m)
+  const branchRegex = new RegExp(/Branch:\s(.+)\n/m)
   const branchMatch = body.match(branchRegex)
   if (!branchMatch) throw Error('No "Branch" found in issue body')
   const branch = branchMatch[1]
